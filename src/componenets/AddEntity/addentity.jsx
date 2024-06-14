3a428c00e905b23829cb15583ca1cf8db4540366
AarinK <aarin.kachroo@gmail.com>
2024-06-14 13:00:13 +0530

    headerInsertionInaddIdentiy

--- src/componenets/AddEntity/addentity.jsx
+++ src/componenets/AddEntity/addentity.jsx
@@ -10,6 +10,13 @@ import SearchIcon from '@mui/icons-material/Search';
 import AddIcon from '@mui/icons-material/Add';
 import styled from '@emotion/styled';
 import {
+    IconButton,
+} from '@mui/material';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+import {
     Box,
     Container,
     Paper,
@@ -116,6 +123,48 @@ function AddEntity() {
         { value: "company", label: "Company" }
     ];
 
+    const AdministrationText = styled(Button)({
+        color: '#1565C0',
+        '&:hover': {
+            textDecoration: 'underline',
+            color: '#1565C0',
+        },
+    });
+    
+    const RightContainer = styled.div(({ sidePanelCollapsed }) => ({
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flex: '1',
+        position: 'absolute',
+        right: 0,
+        top: 0,
+        width: '40%',
+    }));
+    
+    const LeftContainer = styled.div(({ sidePanelCollapsed }) => ({
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        width: '20%',
+        backgroundColor: 'white',
+        borderRadius: '8px',
+        border: '2px solid #ececec',
+        margin: '5px',
+        marginLeft: '1.8%',
+    }));
+    
+    const AccountIcon = styled(AccountCircleIcon)({
+        color: '#613FAA',
+        fontSize: '36px',
+    });
+    
+    const MoreIcon = styled(MoreVertIcon)({
+        color: '#613FAA',
+        fontSize: '36px',
+    });
+    
+
     function handleTypeSelect(option) {
         setSelectedType(option);
         setType(option.label)
@@ -171,13 +220,36 @@ function AddEntity() {
     // function cancel() {
     //     setScreen("Home")
     // }
-
+    
     if (screen === "Home") {
         return <Home />
     }
 
     return (
         <>
+         <div className="headers" style={{
+            display: 'flex',
+            backgroundColor: '#fff',
+            boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)',
+            width: '100%',
+            top: 0,
+            height: '4%',
+            marginLeft: sidePanelCollapsed ? '4%' : '20%',
+        }}>
+            <LeftContainer>
+                <AdministrationText style={{ fontSize: '14px', textTransform: 'capitalize', color: '#71839B' }}>Administration</AdministrationText>
+                <ArrowRightIcon />
+                <AdministrationText style={{ fontSize: '14px', textTransform: 'capitalize' }}>Entity Manager</AdministrationText>
+            </LeftContainer>
+            <RightContainer>
+                <IconButton>
+                    <AccountIcon />
+                </IconButton>
+                <IconButton>
+                    <MoreIcon />
+                </IconButton>
+            </RightContainer>
+        </div>
             <div ref={sidePanelRef} onClick={handleClickOnPanel}>
                 <SidePanel collapsed={sidePanelCollapsed} />
             </div>
